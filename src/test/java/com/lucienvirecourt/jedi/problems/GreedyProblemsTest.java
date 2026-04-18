c53fed2f1c3575144e42891c23e220290792cb98
Syed Ibna Zubayear <syedjubaer997@gmail.com>
2026-04-18 22:28:11 +0600

    tests added for greedy problems

--- src/test/java/com/lucienvirecourt/jedi/problems/GreedyProblemsTest.java
+++ src/test/java/com/lucienvirecourt/jedi/problems/GreedyProblemsTest.java
@@ -40,6 +40,10 @@ class GreedyProblemsTest {
       {5, 7},
       {8, 9},
     }));
+
+    assertTrue(greedyProblems.canAttendMeetings(new int[][]{
+      {4, 8},
+    }));
   }
 
   @Test
@@ -59,6 +63,10 @@ class GreedyProblemsTest {
       {3, 4},
       {1, 3},
     }));
+
+    assertEquals(1, greedyProblems.meetingCount(new int[][]{
+      {2, 6},
+    }));
   }
 
   @Test
@@ -69,11 +77,47 @@ class GreedyProblemsTest {
       {3, 4},
       {1, 3},
     }));
+
+    assertEquals(2, greedyProblems.eraseOverlapIntervals(new int[][]{
+      {1, 4},
+      {2, 5},
+      {3, 6},
+    }));
   }
 
+  @Test
+  void testFindContentChildren() {
+    assertEquals(1, greedyProblems.findContentChildren(new int[]{1, 2, 3}, new int[]{1, 1}));
+    assertEquals(2, greedyProblems.findContentChildren(new int[]{1, 2}, new int[]{1, 2, 3}));
+    assertEquals(0, greedyProblems.findContentChildren(new int[]{}, new int[]{1, 2, 3}));
+  }
 
+  @Test
+  void testLemonadeChange() {
+    assertTrue(greedyProblems.lemonadeChange(new int[]{5, 5, 5, 10, 20}));
+    assertFalse(greedyProblems.lemonadeChange(new int[]{10}));
+    assertFalse(greedyProblems.lemonadeChange(new int[]{5, 5, 10, 10, 20}));
+  }
+
+  @Test
+  void testMaxSubArray() {
+    assertEquals(6, greedyProblems.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    assertEquals(-1, greedyProblems.maxSubArray(new int[]{-3, -2, -1, -4}));
+    assertEquals(7, greedyProblems.maxSubArray(new int[]{7}));
+  }
+
+  @Test
+  void testCanJump() {
+    assertTrue(greedyProblems.canJump(new int[]{2, 3, 1, 1, 4}));
+    assertFalse(greedyProblems.canJump(new int[]{3, 2, 1, 0, 4}));
+    assertTrue(greedyProblems.canJump(new int[]{0}));
+  }
+
+  
   @Test
   void jump() {
     assertEquals(2, greedyProblems.jump(new int[]{2,3,1,1,4}));
+    assertEquals(0, greedyProblems.jump(new int[]{0}));
+    assertEquals(2, greedyProblems.jump(new int[]{2, 3, 0, 1, 4}));
   }
 }
